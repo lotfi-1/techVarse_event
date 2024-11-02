83af69a7275c465b5ee675a570e991c3f5fd7006
lotfi-hallas <lotfihallas111@gmail.com>
2024-11-02 19:36:46 +0100

    add nodemailer

--- utils/nodeMailer.ts
+++ utils/nodeMailer.ts
@@ -1,11 +1,15 @@
+/** @format */
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail", // You can use other email services
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  
-  export default transporter;
+  service: "Gmail", // You can use other email services
+  port: 587, // Use 465 for secure connection (SSL)
+  secure: false, // Set to true if using port 465
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export default transporter;
