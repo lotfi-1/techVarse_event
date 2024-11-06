0049af1805e35fe556d0fa460389a681909975e7
unknown <kadiksalah03@gmail.com>
2024-11-06 21:31:02 +0100

    add missing data

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,11 +1,14 @@
 /** @format */
 import NavBar from "@/components/ui/NavBar";
+import Image from "next/image";
+import logo from "@/public/assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="mx-6 md:mx16 px-5 h-20 flex justify-between items-center gap-x-4">
+    <header className="mx-6 md:mx-16 px-5 h-20 flex justify-between items-center gap-x-4">
       <div className="flex items-center">
-        <h3 className="text-3xl text-primary-txt">Logo</h3>
+        {/* <h3 className="text-3xl text-primary-txt">Logo</h3> */}
+        <Image src={logo} alt="Logo" width={100} height={100} /> {/* Adjust width and height as needed */}
       </div>
       <NavBar />
     </header>
--- lib/data/articles.ts
+++ lib/data/articles.ts
@@ -2,23 +2,27 @@
 
 export const articles: ArticleProps[] = [
   {
-    title: "08:30-09:00",
+    title: "08:30-08:55",
     body: "Check-In",
   },
   {
-    title: "09:00-09:15",
+    title: "09:00-10:25",
     body: "Welcome Word",
   },
   {
-    title: "09:15-10:20",
-    body: "Conference",
+    title: "10:30-11:50",
+    body: "Conference 1 (chergui)",
   },
   {
-    title: "10:20-10:30",
-    body: "Talking",
+    title: "13:00-14:15",
+    body: "Conference 2",
   },
   {
-    title: "10:30-11:00",
-    body: "Closure Ceremony",
+    title: "14:15-14:40",
+    body: "Fun Activity",
+  },
+  {
+    title: "14:40-15:00",
+    body: "Closing The Event",
   },
 ];
