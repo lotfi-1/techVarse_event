ca865b3385f993259f804eaed05233f61c39f210
lotfi-hallas <lotfihallas111@gmail.com>
2024-11-06 20:33:00 +0100

    update information

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -11,11 +11,11 @@ export default function Page() {
       <div className="flex flex-col lg:flex-row lg:justify-evenly gap-x-20 gap-y-[52px] px-6 md:px-16 mt-10 pb-28">
         <div className="max-w-[700px] lg:max-w-[500px]">
           <h3 className="text-[31px] font-bold mb-2">
-            Join the Future of cybersecurity & Innovation
+            Join the Future of cybersecurity
           </h3>
           <p>
             Register now to be a part of the exclusive event where industry
-            leaders, innovators, and enthusiasts converge to shape the future.
+            leaders and enthusiasts converge to shape the future.
           </p>
         </div>
         <div className="w-full flex justify-center items-center">
